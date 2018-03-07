84066de7292327f4f06ad271ae8f7b894d070d92
Duncan McGreggor <duncan.mcgreggor@nasa.gov>
2018-03-06 20:10:46 -0600

    Initial files.

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject hexagram30/shell "0.1.0-SNAPSHOT"
+  :description "A user shell for hexagramMUSH"
+  :url "https://github.com/hexagram30/shell"
+  :license {
+    :name "Apache License, Version 2.0"
+    :url "http://www.apache.org/licenses/LICENSE-2.0"}
+  :dependencies [
+    [org.clojure/clojure "1.8.0"]])
---
+++ test/hexagram30/core_test.clj
@@ -0,0 +1,7 @@
+(ns hexagram30.core-test
+  (:require [clojure.test :refer :all]
+            [hexagram30.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
