035c88fcd8384612d6777394809f796fb96e046f
Duncan McGreggor <oubiwann@gmail.com>
2018-03-10 22:55:40 -0600

    Added more functions to grammar for use in parser.

--- src/hexagram30/shell/grammar.clj
+++ src/hexagram30/shell/grammar.clj
@@ -1,17 +1,17 @@
 (ns hexagram30.shell.grammar)
 
 (def default-command-tree
-  {nil true
-   "" true
+  {"" true
    "bye" true
-   "say" {
-     :any true}})
+   "say" {:depth 1
+          :any true}})
 
 (defn valid-child?
   [result]
   (cond (true? result) true
         (= :any result) true
         (:any result) true
+        (not (nil? (:depth result))) true
         :else false))
 
 (defn valid-parent?
@@ -20,6 +20,36 @@
         (:any result) true
         :else false))
 
+(defn depth-limit
+  [command-tree cmds]
+  (let [full-length (count cmds)]
+    (if-let [depth (:depth (get command-tree (first cmds)))]
+      (if (> depth full-length)
+        full-length
+        depth)
+      full-length)))
+
+(defn get-commands
+  [command-tree cmds]
+  (subvec cmds
+          0
+          (depth-limit command-tree cmds)))
+
+(defn get-tail
+  [command-tree cmds]
+  (subvec cmds
+          (depth-limit command-tree cmds)
+          (count cmds)))
+
+(defn get-subtree
+  [command-tree cmds]
+  (get-in command-tree
+          (get-commands command-tree cmds)))
+
+(defn get-parent-subtree
+  [command-tree cmds]
+  (get-in command-tree (butlast cmds)))
+
 (defn validate
   "Given a collection consisting of a command, an optional sub-command, and
   that sub-commands optional sub-sub-command, etc., check to see if these are
@@ -27,8 +57,7 @@
   ([cmds]
     (validate default-command-tree cmds))
   ([command-tree cmds]
-    (let [child (get-in command-tree cmds)
-          parent (get-in command-tree (butlast cmds))]
+    (let [child (get-subtree command-tree cmds)]
       (if (valid-child? child)
         true
-        (valid-parent? parent)))))
+        (valid-parent? (get-parent-subtree command-tree cmds))))))
--- test/hexagram30/shell/grammar_test.clj
+++ test/hexagram30/shell/grammar_test.clj
@@ -4,18 +4,13 @@
     [hexagram30.shell.grammar :as grammar]))
 
 (def test-command-tree1 {
-  nil true
   "bye" true})
 
 (deftest validate-simple-true
   (is (= true
-         (grammar/validate test-command-tree1 [nil])))
-  (is (= true
          (grammar/validate test-command-tree1 ["bye"]))))
 
 (deftest validate-simple-false
-  (is (= false
-         (grammar/validate test-command-tree1 nil)))
   (is (= false
          (grammar/validate test-command-tree1 [])))
   (is (= false
@@ -27,12 +22,10 @@
 
 (def test-command-tree2 {
   "ps" {"aux" true}
-  "ls" {
-    :any true
-    "-al" {:any true}
-    "-a" {:any true}}
- "rm" {
-  "-rf" {"/tmp/allowed-dir" true}}})
+  "ls" {:any true
+        "-al" {:any true}
+        "-a" {:any true}}
+ "rm" {"-rf" {"/tmp/allowed-dir" true}}})
 
 (deftest validate-nested-true
   (is (= true
@@ -65,3 +58,124 @@
          (grammar/validate test-command-tree2 ["rm" "-rf"])))
   (is (= false
          (grammar/validate test-command-tree2 ["rm" "-rf" "file.txt"]))))
+
+(def test-command-tree3 {
+  "cmd1" {:depth 1}
+  "cmd2" {:depth 2 "subcmd1" true}
+  "cmd3" {:depth 3 "subcmd1" {"subcmd2" true}}
+  "cmd4" {:depth 3 "subcmd1" {"subcmd2" {:any true}}}})
+
+(deftest depth-limit
+  ;; test-command-tree1
+  (is (= 1 (grammar/depth-limit test-command-tree1
+            ["bye"])))
+  (is (= 3 (grammar/depth-limit test-command-tree1
+            ["bye" "bye" "birdie"])))
+  ;; test-command-tree3
+  (is (= 1 (grammar/depth-limit test-command-tree3
+            ["cmd1"])))
+  (is (= 1 (grammar/depth-limit test-command-tree3
+            ["cmd1" "x" "y" "z"])))
+  (is (= 1 (grammar/depth-limit test-command-tree3
+            ["cmd2"])))
+  (is (= 2 (grammar/depth-limit test-command-tree3
+            ["cmd2" "subcmd1"])))
+  (is (= 2 (grammar/depth-limit test-command-tree3
+            ["cmd2" "subcmd1" "thing"])))
+  (is (= 1 (grammar/depth-limit test-command-tree3
+            ["cmd3"])))
+  (is (= 2 (grammar/depth-limit test-command-tree3
+            ["cmd3" "subcmd1"])))
+  (is (= 3 (grammar/depth-limit test-command-tree3
+            ["cmd3" "subcmd1" "subcmd2"])))
+  (is (= 3 (grammar/depth-limit test-command-tree3
+            ["cmd4" "subcmd1" "subcmd2" "and" "more" "things"]))))
+
+(deftest get-commands
+  ;; test-command-tree1
+  (is (= ["bye"]
+         (grammar/get-commands test-command-tree1
+          ["bye"])))
+  (is (= ["bye" "bye" "birdie"]
+         (grammar/get-commands test-command-tree1
+          ["bye" "bye" "birdie"])))
+  ;; test-command-tree3
+  (is (= ["cmd1"]
+         (grammar/get-commands test-command-tree3
+          ["cmd1"])))
+  (is (= ["cmd1"]
+         (grammar/get-commands test-command-tree3
+          ["cmd1" "x" "y" "z"])))
+  (is (= ["cmd2"]
+         (grammar/get-commands test-command-tree3
+          ["cmd2"])))
+  (is (= ["cmd2" "subcmd1"]
+         (grammar/get-commands test-command-tree3
+          ["cmd2" "subcmd1"])))
+  (is (= ["cmd2" "subcmd1"]
+         (grammar/get-commands test-command-tree3
+          ["cmd2" "subcmd1" "thing"])))
+  (is (= ["cmd3"]
+         (grammar/get-commands test-command-tree3
+          ["cmd3"])))
+  (is (= ["cmd3" "subcmd1"]
+         (grammar/get-commands test-command-tree3
+          ["cmd3" "subcmd1"])))
+  (is (= ["cmd3" "subcmd1" "subcmd2"]
+         (grammar/get-commands test-command-tree3
+          ["cmd3" "subcmd1" "subcmd2"])))
+  (is (= ["cmd4" "subcmd1" "subcmd2"]
+         (grammar/get-commands test-command-tree3
+          ["cmd4" "subcmd1" "subcmd2" "and" "more" "things"]))))
+
+(deftest get-tail
+  (is (= []
+         (grammar/get-tail test-command-tree3
+          ["cmd1"])))
+  (is (= ["x" "y" "z"]
+         (grammar/get-tail test-command-tree3
+          ["cmd1" "x" "y" "z"])))
+  (is (= []
+         (grammar/get-tail test-command-tree3
+          ["cmd3" "subcmd1" "subcmd2"])))
+  (is (= ["and" "yet" "more" "things"]
+         (grammar/get-tail test-command-tree3
+          ["cmd4" "subcmd1" "subcmd2" "and" "yet" "more" "things"]))))
+
+(deftest get-subtree
+  ;; test-command-tree1
+  (is (= true
+         (grammar/get-subtree test-command-tree1
+          ["bye"])))
+  (is (= nil
+         (grammar/get-subtree test-command-tree1
+          ["bye" "bye" "birdie"])))
+  ;; test-command-tree3
+  (is (= {:depth 1}
+         (grammar/get-subtree test-command-tree3
+          ["cmd1"]))))
+
+(deftest validate-nested-depth-check-true
+  (is (= true
+         (grammar/validate test-command-tree3
+                           ["cmd1"])))
+  (is (= true
+         (grammar/validate test-command-tree3
+                           ["cmd1" "the" "rest" "is" "ignored"])))
+  (is (= true
+         (grammar/validate test-command-tree3
+                           ["cmd2" "subcmd1"])))
+  (is (= true
+         (grammar/validate test-command-tree3
+                           ["cmd2" "subcmd1" "the" "rest" "is" "ignored"]))))
+
+(deftest validate-nested-depth-check-false
+  (is (= false
+         (grammar/validate test-command-tree3
+                           ["cmd"])))
+  (is (= false
+         (grammar/validate test-command-tree3
+                           ["cmd2" "the" "rest" "is" "ignored"])))
+  (is (= false
+         (grammar/validate test-command-tree3
+                           ["cmd2" "subcmd" "the" "rest" "is" "ignored"]))))
