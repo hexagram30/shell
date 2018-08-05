3ad4130b11f0d398ff020b020ca79e726880e531
Duncan McGreggor <oubiwann@gmail.com>
2018-08-04 23:24:00 -0500

    Added Travis CI support.

--- project.clj
+++ project.clj
@@ -78,4 +78,13 @@
       ;["eastwood"]
       ]
     "ltest"
-      ["with-profile" "+test" "ltest"]})
+      ["with-profile" "+test" "ltest"]
+    "ltest-clean" ["do"
+      ["clean"]
+      ["ltest"]]
+    "build" ["do"
+      ["clean"]
+      ["check-vers"]
+      ["lint"]
+      ["ltest" ":all"]
+      ["uberjar"]]})
