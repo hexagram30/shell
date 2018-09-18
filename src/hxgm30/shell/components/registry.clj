b3e0933197d70d58e7e99364d54cac9d7c4dd89d
Duncan McGreggor <oubiwann@gmail.com>
2018-09-18 00:50:55 -0500

    Added command-tree to registry.

--- src/hxgm30/shell/components/registry.clj
+++ src/hxgm30/shell/components/registry.clj
@@ -2,6 +2,7 @@
   (:require
    [hxgm30.shell.config :as shell-config]
    [hxgm30.shell.core :as shell]
+   [hxgm30.shell.reader.grammar :as grammar]
    [com.stuartsierra.component :as component]
    [taoensso.timbre :as log]))
 
@@ -15,6 +16,10 @@
 ;;;   Registry Component API   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(defn command-tree
+  [system shell]
+  (get-in system [:grammar :command-tree shell]))
+
 (defn get-shell
   [system shell]
   (get-in system [:shell :registry shell]))
@@ -23,12 +28,15 @@
 ;;;   Component Lifecycle Implementation   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defrecord ShellRegistry [registry])
+(defrecord ShellRegistry [
+  command-tree
+  registry])
 
 (defn start
   [this]
   (log/info "Starting shell registry component ...")
   (log/debug "Started shell registry component.")
+  (assoc-in this [:grammar :command-tree] grammar/command-tree)
   (assoc-in this [:registry :entry] (shell/create :entry)))
 
 (defn stop
