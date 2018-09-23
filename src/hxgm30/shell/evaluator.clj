f0ec2d4ac516cdd54650dd98444d9a6db04a3f13
Duncan McGreggor <oubiwann@gmail.com>
2018-09-23 12:35:41 -0500

    Updated evaluator to support subcommand help.

--- src/hxgm30/shell/evaluator.clj
+++ src/hxgm30/shell/evaluator.clj
@@ -7,26 +7,40 @@
 
 (defn commands
   [^Keyword shell]
-  (->> shell
-       grammar/commands
-       keys
-       sort
-       (map name)))
+  (str formatter/new-line
+       (grammar/commands shell
+                         {:as-item-list true
+                          :prefix-text (str "Available commands:"
+                                            formatter/new-line)})
+       formatter/new-line
+       (formatter/paragraph (str "To view the help for a command, "
+                                 "type 'help <COMMAND>'."))))
 
 (defn help
   [^Keyword shell str-args]
   (let [[cmd & subcmds] (map keyword str-args)]
     (cond (nil? cmd)
-          {:error :command-not-provided}
+          (commands shell)
 
           (not (grammar/has-command? shell cmd))
           {:error :command-not-cound}
 
-          (not (apply grammar/has-subcommand? [shell cmd subcmds]))
+          (not (apply grammar/has-subcommands? shell cmd subcmds))
           {:error :subcommand-not-found}
 
           :else
-          (let [help-text (apply grammar/subcommand-help
-                                 [shell cmd subcmds])]
+          (let [help-text (grammar/subcommand-help shell cmd subcmds)]
             (str formatter/new-line
-                 (formatter/paragraph help-text))))))
+                 (formatter/paragraph help-text)
+                 (when (grammar/has-subcommands? shell cmd subcmds)
+                  (grammar/subcommands
+                   shell
+                   cmd
+                   subcmds
+                   {:as-item-list true
+                    :prefix-text (str "Available subcommands:"
+                                      formatter/new-line)}))
+                 formatter/new-line
+                 (formatter/paragraph
+                  (str "To view the help for a subcommand, type "
+                       "'help <COMMAND> <SUBCOMMAND>'")))))))
